1a281602040c6aaf999dfd3b1ccecb7acedb88b9
Kimberley Polypheme <kim.polypheme@gmail.com>
2023-12-04 12:42:38 +0100

    Version final du filtre

--- app/models/program.rb
+++ app/models/program.rb
@@ -13,8 +13,7 @@ class Program < ApplicationRecord
   validates :allergies, presence: true
   validates :nb_of_meals_by_day, presence: true, inclusion: { in: [2, 3, 4] }
   validates :nb_of_days, presence: true, inclusion: { in: 2..7 }
-  validates :nb_of_snacks, presence: true, inclusion: { in: 0..3 }
-  validates :height, presence: true
-  validates :weight, presence: true
+  
+
   validates :sexe, presence: true, inclusion: { in: ["Male", "Female"] }
 end

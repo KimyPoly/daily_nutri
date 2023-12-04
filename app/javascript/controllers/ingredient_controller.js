5e5be74ec43e6781f67a061445f54907cb98a1de
sonya bouafia <bouafiasonya@yahoo.com>
2023-12-04 15:14:49 +0100

    list de courses ok

--- app/javascript/controllers/ingredient_controller.js
+++ app/javascript/controllers/ingredient_controller.js
@@ -2,28 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="ingredient"
 export default class extends Controller {
-  static targets = ["ingredient"];
   connect() {
-    this.updateCheckedIngredients();
-  }
-  updateCheckedIngredients() {
-    this.ingredientTargets.forEach(ingredient => {
-      const checkbox = ingredient.querySelector(".ingredient-checkbox");
-      checkbox.checked = ingredient.classList.contains("checked");
-    });
   }
 
   toggleChecked(event) {
-    const ingredient = event.currentTarget;
-    ingredient.classList.toggle("checked");
-
-    // Récupérer l'ID ou le nom de l'ingrédient pour envoyer au backend
-    const ingredientName = ingredient.dataset.ingredient;
-    // Effectuer une requête AJAX ou une action pour mettre à jour l'état dans le backend
-    // Utilisez cette information pour marquer l'ingrédient comme acheté
-
-    // Mettre à jour l'état des ingrédients cochés
-    this.updateCheckedIngredients();
+    console.log(event.currentTarget.nextElementSibling);
+    event.currentTarget.nextElementSibling.classList.toggle('ingre-select')
   }
 }
-

58b267cf4c35cfad6c9e169e1b49e427eff78d94
louis leveque <louis.leveque.nsi@gmail.com>
2023-12-05 12:49:43 +0100

    calendar bubble & calories

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -18,7 +18,6 @@ class ProgramsController < ApplicationController
     return unless @breakfasts.count > @program.nb_of_days
 
     @breakfasts.each_with_index { |breakfast, index| @dishes.push(breakfast) if index > (@program.nb_of_days - 1) }
-
     @meals = current_user.programs.last.meals
   end
 
--- app/models/program.rb
+++ app/models/program.rb
@@ -1,4 +1,6 @@
 class Program < ApplicationRecord
+  attr_accessor :objectif
+
   GOAL = ["Lose weight", "Weight maintenance", "Gain weight"]
   DIET = ["Vegan", "Vegetarian", "Omnivorous", "Pescatarian", "Keto"]
   ALLERGIES = ["None", "Nuts", "Crusta", "Milk", "Eggs", "Fish", "Crustacean",
@@ -13,7 +15,7 @@ class Program < ApplicationRecord
   validates :allergies, presence: true
   validates :nb_of_meals_by_day, presence: true, inclusion: { in: [2, 3, 4] }
   validates :nb_of_days, presence: true, inclusion: { in: 2..7 }
-  
+
 
   validates :sexe, presence: true, inclusion: { in: ["Male", "Female"] }
 
@@ -21,25 +23,50 @@ class Program < ApplicationRecord
     meals.pluck(:calories).sum
   end
 
-  def calories_goal
-    @genre = sexe
-    @asked_goal = goal
-    @calories_goal = case
-    when @genre == "Male" && @asked_goal == "Weight maintenance"
-      @calories_goal = 2600
-    when @genre == "Male" && @asked_goal == "Lose weight"
-      @calories_goal = 2300
-    when @genre == "Male" && @asked_goal == "Take weight"
-      @calories_goal = 3100
-    when @genre == "Female" && @asked_goal == "Weight maintenance"
-      @calories_goal = 2000
-    when @genre == "Female" && @asked_goal == "Lose weight"
-      @calories_goal = 1800
-    when @genre == "Female" && @asked_goal == "Take weight"
-      @calories_goal = 2500
+  # def calories_goal
+  #   @genre = sexe
+  #   @asked_goal = goal
+  #   @calories_goal = case
+  #   when @genre == "Male" && @asked_goal == "Weight maintenance"
+  #     @calories_goal = 2600
+  #   when @genre == "Male" && @asked_goal == "Lose weight"
+  #     @calories_goal = 2300
+  #   when @genre == "Male" && @asked_goal == "Take weight"
+  #     @calories_goal = 3100
+  #   when @genre == "Female" && @asked_goal == "Weight maintenance"
+  #     @calories_goal = 2000
+  #   when @genre == "Female" && @asked_goal == "Lose weight"
+  #     @calories_goal = 1800
+  #   when @genre == "Female" && @asked_goal == "Take weight"
+  #     @calories_goal = 2500
+  #   else
+  #     0
+  #   end
+  #   return @calories_goal * nb_of_days
+  # end
+
+  def fix_cal
+    weight = 65
+    height = 1.80
+    age = 18
+    activite = 1.2
+    if sexe == "Female"
+      @mb = (655 + (9.6 * weight) + (500 * height) - (4.7 * age)) * activite
     else
-      0
+      @mb = (66 + (13.7 * weight) + (185 * height) - (6.5 * age)) * activite
     end
-    return @calories_goal * nb_of_days
+  end
+
+  def calories_goal
+    fix_cal
+    case goal
+    when "Lose weight"
+      @calories_goal = @mb - 500
+    when "Weight maintenance"
+      @calories_goal = @mb
+    when "Gain weight"
+      @calories_goal = @mb + 500
+    end
+    return (@calories_goal * nb_of_days).to_i
   end
 end

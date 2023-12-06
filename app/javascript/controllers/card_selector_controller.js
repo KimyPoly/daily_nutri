3fa9a2a9558baf3a1faa026bdff2c3bc35cd233d
sonya bouafia <bouafiasonya@yahoo.com>
2023-12-06 21:02:26 +0100

    carte clickable

--- app/javascript/controllers/card_selector_controller.js
+++ app/javascript/controllers/card_selector_controller.js
@@ -2,25 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="card-selector"
 export default class extends Controller {
-  static targets = ["card"];
 
   connect() {
 
   }
 
-  toggleStyle(event) {
-    // console.log(event.currentTarget);
-    event.currentTarget.classList.toggle('selected')
-    // const checkbox = this.element.querySelector('.hidden-checkbox');
+  toggleStyle() {
+    this.cardTargets.forEach(card => {
+      card.classList.add("active");
 
-    // if (checkbox.checked) {
-    //   this.cardTargets.forEach(card => {
-    //     card.classList.add('selected');
-    //   });
-    // } else {
-    //   this.cardTargets.forEach(card => {
-    //     card.classList.remove('selected');
-    //   });
-    // }
+      const programPath = card.dataset.programPath;
+      if (programPath) {
+        window.location.href = programPath;
+      }
+    });
   }
 }
